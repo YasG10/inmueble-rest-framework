b11b6e7b8f805cc52ba482578cdf1557d70f00b5
yasG <yasseryasg@gmail.com>
2024-03-19 15:20:02 -0400

    segundo commit

--- inmuebles/settings.py
+++ inmuebles/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "inmuebleslist_app",
     "rest_framework",
+    "rest_framework.authtoken",
 ]
 
 MIDDLEWARE = [
@@ -129,7 +130,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 }"""
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
+    #'DEFAULT_AUTHENTICATION_CLASSES': [
+    #    'rest_framework.authentication.BasicAuthentication',
+    # ]
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
     ]
 }
