8a05d4a7862762a61957144ab2a47ad29ca30d16
yasG <yasseryasg@gmail.com>
2024-03-19 15:45:54 -0400

    tercer c commit

--- inmuebles/urls.py
+++ inmuebles/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("tienda/", include("inmuebleslist_app.api.urls")),
-    path("api-auth/", include("rest_framework.urls")),
+    path("account/", include("user_app.api.urls")),
+    #path("api-auth/", include("rest_framework.urls")),
 ]
